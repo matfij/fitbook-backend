78cd1f8f6e0015834c81d1aa15bf09dc149f1dbd
matfij <mateusz.michal.fijak@gmail.com>
2021-03-28 21:39:04 +0200

    Customized Django admin

--- app/api_core/admin.py
+++ app/api_core/admin.py
@@ -1,3 +1,27 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext
+from api_core import models
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (gettext('Personal Info'), {'fields': ('name',)}),
+        (
+            gettext('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_admin')}
+        ),
+        (gettext('Statistics'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (
+            None,
+            {'classes': ('wide',), 'fields': ('email','password1', 'password2')}
+    	),
+    )
+
+
+admin.site.register(models.User, CustomUserAdmin)
--- app/api_core/models.py
+++ app/api_core/models.py
@@ -23,6 +23,7 @@ class UserManager(BaseUserManager):
         '''Creates and saves a new administrator'''
         user = self.create_user(email, password)
         user.is_admin = True
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
@@ -35,6 +36,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
