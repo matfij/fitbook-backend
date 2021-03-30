20c122fb60675ff04f717271aeb8083fd8d200dc
matfij <mateusz.michal.fijak@gmail.com>
2021-03-30 12:09:41 +0200

    Improved app safety

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,16 +12,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+ss13d9ct$ajrt%6t0ty#o$vgq2-f_zn%arjnfez8bayfkg_9$'
+# SECURITY WARNING: keep the secret key used in production secret! 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
